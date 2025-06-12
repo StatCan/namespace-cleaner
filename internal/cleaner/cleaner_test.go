c15226806c3eec351eb18b848af2dec6098ccdff
Bryan Paget <8212170+bryanpaget@users.noreply.github.com>
2025-06-12 15:33:35 -0400

    chore(cleaner_test): add clienttesting

--- internal/cleaner/cleaner_test.go
+++ internal/cleaner/cleaner_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	clienttesting "k8s.io/client-go/testing"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes/fake"
